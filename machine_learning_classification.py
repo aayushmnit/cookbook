e1d04dc9489019bc3e5b3b56229b06325842a3d2
agraw134 <agraw134@umn.edu>
2018-02-10 20:00:45 -0600

    Modifying machine_learning_classification.py

--- machine_learning_classification.py
+++ machine_learning_classification.py
@@ -11,12 +11,15 @@ from sklearn.model_selection import KFold, train_test_split ## Creating cross va
 from sklearn import metrics ## For loss functions
 
 ## Libraries for Classification algorithms
-from ensemble import ExtraTreesClassifier  ## ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from ensemble import ExtraTreesClassifier,RandomForestClassifier
 import xgboost as xgb ## Xgboost for regression and classfication
 import lightgbm as lgb ## Light GBM for regression and classification
 
-
-## For classification
+####### Algorithms For Binary classification #########
  
 ### Running Xgboost
 def create_feature_map(features):
@@ -67,11 +70,11 @@ def runXGB(train_X, train_y, test_X, test_y=None, test_X2=None, feature_names=No
 	loss = 0
 	if test_y is not None:
 		loss = metrics.roc_auc_score(test_y, pred_test_y)
-		return pred_test_y, loss, pred_test_y2
+		return pred_test_y, loss, pred_test_y2, model
 	else:
-		return pred_test_y, loss, pred_test_y2
+		return pred_test_y, loss, pred_test_y2, model
 
-## Running LightGBM
+### Running LightGBM
 def runLGB(train_X, train_y, test_X, test_y=None, test_X2=None, feature_names=None, seed_val=0, rounds=500, dep=8, eta=0.05):
 	params = {}
 	params["objective"] = "binary"
@@ -103,19 +106,19 @@ def runLGB(train_X, train_y, test_X, test_y=None, test_X2=None, feature_names=No
 	if test_y is not None:
 		loss = metrics.roc_auc_score(test_y, pred_test_y)
 		print loss
-		return pred_test_y, loss, pred_test_y2
+		return pred_test_y, loss, pred_test_y2, model
 	else:
-		return pred_test_y, loss, pred_test_y2
+		return pred_test_y, loss, pred_test_y2, model
 
-## Running Extra Trees  
+### Running Extra Trees  
 def runET(train_X, train_y, test_X, test_y=None, test_X2=None, depth=20, leaf=10, feat=0.2):
-	model = ensemble.ExtraTreesClassifier(
+	model = ExtraTreesClassifier(
 			n_estimators = 100,
 					max_depth = depth,
 					min_samples_split = 2,
 					min_samples_leaf = leaf,
 					max_features =  feat,
-					n_jobs = 6,
+					n_jobs = 8,
 					random_state = 0)
 	model.fit(train_X, train_y)
 	train_preds = model.predict_proba(train_X)[:,1]
@@ -127,4 +130,122 @@ def runET(train_X, train_y, test_X, test_y=None, test_X2=None, depth=20, leaf=10
 		test_loss = metrics.roc_auc_score(test_y, test_preds)
 		print "Depth, leaf, feat : ", depth, leaf, feat
 		print "Train and Test loss : ", train_loss, test_loss
-	return test_preds, test_loss, test_preds2
+	return test_preds, test_loss, test_preds2, model
+ 
+ ### Running Random Forest
+ def runRF(train_X, train_y, test_X, test_y=None, test_X2=None, depth=20, leaf=10, feat=0.2):
+    model = ensemble.RandomForestClassifier(
+            n_estimators = 1000,
+                    max_depth = depth,
+                    min_samples_split = 2,
+                    min_samples_leaf = leaf,
+                    max_features =  feat,
+                    n_jobs = 4,
+                    random_state = 0)
+    model.fit(train_X, train_y)
+    train_preds = model.predict_proba(train_X)[:,1]
+    test_preds = model.predict_proba(test_X)[:,1]
+    test_preds2 = model.predict_proba(test_X2)[:,1]
+    test_loss = 0
+    
+    train_loss = metrics.log_loss(train_y, train_preds)
+    test_loss = metrics.log_loss(test_y, test_preds)
+    print "Train and Test loss : ", train_loss, test_loss
+    return test_preds, test_loss, test_preds2, model
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+ ### Running Logistic Regression
+ def runLR(train_X, train_y, test_X, test_y=None, test_X2=None, C=1.0, penalty ='l1'):
+    model = LogisticRegression(C=C, penalty=penalty, n_jobs=-1)
+    model.fit(train_X, train_y)
+    train_preds = model.predict_proba(train_X)[:,1]
+    test_preds = model.predict_proba(test_X)[:,1]
+    test_preds2 = model.predict_proba(test_X2)[:,1]
+    test_loss = 0
+    
+    train_loss = metrics.log_loss(train_y, train_preds)
+    test_loss = metrics.log_loss(test_y, test_preds)
+    print "Train and Test loss : ", train_loss, test_loss
+    return test_preds, test_loss, test_preds2, model
+
+### Running Decision Tree
+def runDT(train_X, train_y, test_X, test_y=None, test_X2=None, criterion='gini', depth=None, min_split=2, min_leaf=1):
+    model = DecisionTreeClassifier(criterion = criterion, max_depth = depth, min_samples_split = min_split, min_samples_leaf=min_leaf)
+    model.fit(train_X, train_y)
+    train_preds = model.predict_proba(train_X)[:,1]
+    test_preds = model.predict_proba(test_X)[:,1]
+    test_preds2 = model.predict_proba(test_X2)[:,1]
+    test_loss = 0
+    
+    train_loss = metrics.log_loss(train_y, train_preds)
+    test_loss = metrics.log_loss(test_y, test_preds)
+    print "Train and Test loss : ", train_loss, test_loss
+    return test_preds, test_loss, test_preds2, model
+    
+### Running K-Nearest Neighbour
+def runKNN(train_X, train_y, test_X, test_y=None, test_X2=None, neighbors=5):
+    model = KNeighborsClassifier(n_neighbors=neighbors, n_jobs=-1)
+    model.fit(train_X, train_y)
+    train_preds = model.predict_proba(train_X)[:,1]
+    test_preds = model.predict_proba(test_X)[:,1]
+    test_preds2 = model.predict_proba(test_X2)[:,1]
+    test_loss = 0
+    
+    train_loss = metrics.log_loss(train_y, train_preds)
+    test_loss = metrics.log_loss(test_y, test_preds)
+    print "Train and Test loss : ", train_loss, test_loss
+    return test_preds, test_loss, test_preds2, model
+
+### Running K-Nearest Neighbour
+def runSVC(train_X, train_y, test_X, test_y=None, test_X2=None, C=1.0, kernel_choice = 'rbf'):
+    model = SVC(C=C, kernel='kernel_choice', probability=True)
+    model.fit(train_X, train_y)
+    train_preds = model.predict_proba(train_X)[:,1]
+    test_preds = model.predict_proba(test_X)[:,1]
+    test_preds2 = model.predict_proba(test_X2)[:,1]
+    test_loss = 0
+    
+    train_loss = metrics.log_loss(train_y, train_preds)
+    test_loss = metrics.log_loss(test_y, test_preds)
+    print "Train and Test loss : ", train_loss, test_loss
+    return test_preds, test_loss, test_preds2, model
+
+########### Cross Validation #######################
+### 1) Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=46)
+pred_val, loss, pred_test, model = runXGB(train_X = X_train, train_y = y_train,
+                                          test_X = X_test, test_y = y_test,
+                                          rounds=5000, dep=8)
+print("K-Fold Loss = {0}".format(np.mean(loss_list)))
+
+### 2) Cross-Validation (K-Fold)
+cv = cross_validation.KFold(len(X), n_folds=5, shuffle=True, random_state=46)
+loss_list = []
+for traincv, testcv in cv:
+    pred_val, loss, pred_test, model = runXGB(train_X = X.ix[traincv,:], train_y = y[traincv],
+                                              test_X = X.ix[testcv,:], test_y = y[testcv],
+                                              rounds=5000, dep=8)
+    loss_list.append(loss)
+print("K-Fold Loss = {0}".format(np.mean(loss_list)))
+
+## Variable Importance plot
+def feature_importance(model):
+    """
+    Plots the feature importance for an ensemble model
+    """
+    feature_importance = model.feature_importances_
+    feature_importance = 100.0 * (feature_importance / feature_importance.max())
+    sorted_idx = np.argsort(feature_importance)
+    pos = np.arange(sorted_idx.shape[0]) + .5
+    plt.figure(figsize=(15, 15))
+    plt.subplot(1, 2, 2)
+    plt.barh(pos, feature_importance[sorted_idx], align='center')
+    plt.yticks(pos, X.columns[sorted_idx])
+    plt.xlabel('Relative Importance')
+    plt.title('Variable Importance')
+    plt.show()
+
