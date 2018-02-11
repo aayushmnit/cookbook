ef9b48ddf25bb1c8d2cf22dba05fe52464e03e51
agraw134 <agraw134@umn.edu>
2018-02-11 12:58:15 -0600

    Adding missing value treatment function to generic preprocessing

--- generic_preprocessing.py
+++ generic_preprocessing.py
@@ -78,6 +78,70 @@ def drop_allsame(df):
             to_drop.append(i)
     return df.drop(to_drop,axis =1)
 
+def treat_missing_numeric(df,columns,how = 'mean'):
+    '''
+    Function to treat missing values in numeric columns
+    Required Input - 
+        - df = Pandas DataFrame
+        - columns = List input of all the columns need to be imputed
+        - how = valid values are 'mean', 'mode', 'median','ffill', numeric value
+    Expected Output -
+        - Pandas dataframe with imputed missing value in mentioned columns
+    '''
+    if how == 'mean':
+        for i in columns:
+            print("Filling missing values with mean for columns - {0}".format(i))
+            df.ix[:,i] = df.ix[:,i].fillna(df.ix[:,i].mean())
+            
+    elif how == 'mode':
+        for i in columns:
+            print("Filling missing values with mode for columns - {0}".format(i))
+            df.ix[:,i] = df.ix[:,i].fillna(df.ix[:,i].mode())
+    
+    elif how == 'median':
+        for i in columns:
+            print("Filling missing values with median for columns - {0}".format(i))
+            df.ix[:,i] = df.ix[:,i].fillna(df.ix[:,i].median())
+    
+    elif how == 'ffill':
+        for i in columns:
+            print("Filling missing values with forward fill for columns - {0}".format(i))
+            df.ix[:,i] = df.ix[:,i].fillna(method ='ffill')
+    
+    elif type(how) == int or type(how) == float:
+        for i in columns:
+            print("Filling missing values with {0} for columns - {1}".format(how,i))
+            df.ix[:,i] = df.ix[:,i].fillna(how)
+    else:
+        print("Missing value fill cannot be completed")
+    return df
+
+def treat_missing_categorical(df,columns,how = 'mode'):
+    '''
+    Function to treat missing values in numeric columns
+    Required Input - 
+        - df = Pandas DataFrame
+        - columns = List input of all the columns need to be imputed
+        - how = valid values are 'mode', any string or numeric value
+    Expected Output -
+        - Pandas dataframe with imputed missing value in mentioned columns
+    '''
+    if how == 'mode':
+        for i in columns:
+            print("Filling missing values with mode for columns - {0}".format(i))
+            df.ix[:,i] = df.ix[:,i].fillna(df.ix[:,i].mode()[0])
+    elif type(how) == str:
+        for i in columns:
+            print("Filling missing values with {0} for columns - {1}".format(how,i))
+            df.ix[:,i] = df.ix[:,i].fillna(how)
+    elif type(how) == int or type(how) == float:
+        for i in columns:
+            print("Filling missing values with {0} for columns - {1}".format(how,i))
+            df.ix[:,i] = df.ix[:,i].fillna(str(how))
+    else:
+        print("Missing value fill cannot be completed")
+    return df
+    
 def min_max_scaler(df,columns):
     '''
     Function to do Min-Max scaling
